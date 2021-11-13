51f31bf0b7fedc973360a76cb6663387a5ea240c
nhan.trinh <hongnhan50@gmail.com>
2021-11-13 20:05:16 +0700

    homepage

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -10,6 +10,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
 @NgModule({
     imports: [
         MatButtonModule,
@@ -22,7 +23,8 @@ import { MatListModule } from '@angular/material/list';
         MatCheckboxModule,
         MatSidenavModule,
         MatToolbarModule,
-        MatListModule
+        MatListModule,
+        MatTabsModule
     ],
     exports: [
         MatButtonModule,
@@ -35,7 +37,8 @@ import { MatListModule } from '@angular/material/list';
         MatCheckboxModule,
         MatSidenavModule,
         MatToolbarModule,
-        MatListModule
+        MatListModule,
+        MatTabsModule
     ]
 })
 
